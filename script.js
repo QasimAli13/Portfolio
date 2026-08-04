ff1833846076a578c68ccab20c8f9f87b6d6a06c
QasimAli13 <qa4451226@gmail.com>
2026-08-04 14:51:01 +0500

    updated skills

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const darkModeBtn = document.getElementById("dark-mode-btn");
 
 darkModeBtn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
-        darkModeBtn.textContent = "☀️";
-    } else {
-        darkModeBtn.textContent = "🌙";
-    }
+  if (document.body.classList.contains("dark-mode")) {
+    darkModeBtn.textContent = "☀️";
+  } else {
+    darkModeBtn.textContent = "🌙";
+  }
+});
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll("#nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
